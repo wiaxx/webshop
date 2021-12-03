4a5b6928994366b06ee10b4b7b12b48ec3040e4f
Amanda <amanda.wikstrom@hotmail.com>
2021-12-03 16:41:26 +0100

    fix shopcart.js shopcart.html

--- shopcart.js
+++ shopcart.js
@@ -2,41 +2,56 @@ window.addEventListener('load', getShopCart);
 
 let totalSum = 0;
 
+// function to show products in shop cart from localStorage
 function getShopCart() {
     const ls = localStorage.getItem("shoppingCart");
     const shopCart = JSON.parse(ls);
 
+    const wishListBtn = document.querySelector(".wishBtn");
+    const shopCartBtn = document.querySelector(".cart");
     const h2 = document.querySelector("h2");
+
     if (localStorage.getItem("shoppingCart") !== null) {
         h2.style.display = "none";
-    }
+    };
+    //make wishlist button red if products in it
+    if (localStorage.getItem("wishlist") === "[]") {
+        wishListBtn.style.color = "black";
+    } else {
+        wishListBtn.style.color = "red";
+    };
+    // make shoppingcart green if products in it
+    if (localStorage.getItem("shoppingCart") == "[]") {
+        shopCartBtn.style.color = "black";
+    } else {
+        shopCartBtn.style.color = "rgb(99, 158, 99)";
+    };
 
     shopCart.forEach(element => {
-        // console.log(element);
         const div = document.createElement("div");
         div.setAttribute("id", `${element.id}`);
         div.classList.add("scDiv")
+
         const img = document.createElement("img");
         img.setAttribute("src", `${element.img}`);
         img.classList.add("prodImg");
+
         const contentDiv = document.createElement("div");
         contentDiv.classList.add("contentDiv");
+
         const h1 = document.createElement("h1");
         h1.innerText = element.name;
+
         const descP = document.createElement("p");
         descP.innerText = element.desc;
+
         const span = document.createElement("span");
         span.innerText = `${element.price}:-`;
-        // const buyBtn = document.createElement("button")
-        // buyBtn.classList.add("buyBtn");
-        // buyBtn.innerText = "Add to cart";
-        // const wishlistBtn = document.createElement("button");
-        // wishlistBtn.setAttribute("id", `${element.id}`);
-        // wishlistBtn.classList.add("wlBtn");
-        // wishlistBtn.innerHTML = '<i class="far fa-heart"></i>';
+
         const remove = document.createElement("button");
         remove.innerText = "x";
         remove.classList.add("removeBtn");
+
         const br = document.createElement("br");
 
         contentDiv.append(h1, descP, span);
@@ -45,7 +60,8 @@ function getShopCart() {
         cartPage.appendChild(div);
 
         totalSum += Number(element.price);
-    })
+    });
+
     const orderBtn = document.createElement("button");
     orderBtn.classList.add("placeOrder");
     orderBtn.innerText = "Continue with order";
@@ -61,21 +77,22 @@ function getShopCart() {
     document.querySelector(".placeOrder").addEventListener('click', placeOrder);
 };
 
+// function to remove item from shopCart and change tot items and sum
 function removeItem(e) {
-    console.log(e);
     const element = e.target.parentElement;
-    console.log(element);
+
     const ls = localStorage.getItem("shoppingCart");
     const savedProducts = JSON.parse(ls);
 
     const index = savedProducts.findIndex(x => x.id == e.target.parentElement.id)
-    console.log(index);
 
     const shopItem = savedProducts.splice(index, 1);
-    console.log(shopItem);
-    console.log(savedProducts)
-    console.log(shopItem[0].price);
+    // console.log(shopItem);
+    // console.log(savedProducts)
+    // console.log(shopItem[0].price);
 
+    const totProd = document.querySelector(".totprod");
+    totProd.innerText = `Total products: ${savedProducts.length}`;
 
     totalSum -= Number(shopItem[0].price);
     const totSum = document.querySelector(".sum");
@@ -83,9 +100,8 @@ function removeItem(e) {
 
     localStorage.setItem("shoppingCart", JSON.stringify(savedProducts));
     element.remove();
-
-}
+};
 
 function placeOrder() {
-    
-}
+
+};
